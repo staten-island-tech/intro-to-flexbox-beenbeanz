0b30e3a77af7b7efc36e6dd513475844b7336ed7
Hailey Lan <haileylan404@gmail.com>
2025-09-25 10:52:44 -0700

    thurs

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-function money(paint, caps, pokedollars){
+/*function money(paint, caps, pokedollars){
     const remainingPaint = paint % caps;
     const bottleCapsPainted = Math.floor(paint/caps);
     const dollarsMade = bottleCapsPainted * pokedollars;
     return dollarsMade + remainingPaint;
 }
-console.log(money(14,3,10));
+console.log(money(14,3,10));*/
+
+
+//PROBLEM 2: HOUSE ALARM
+/*const doorOpen = true;
+const windowOpen = true;
+
+function houseAlarm(doorOpen, windowOpen){
+    const alarmSilent = !(doorOpen || windowOpen);
+    return alarmSilent
+}
+console.log(houseAlarm(doorOpen, windowOpen));
+
+//PROBLEM 1:
+const rideHeightOK = true;
+const withAdult = true;
+const healthHold = true;
+
+function houseAlarm(rideHeightOK, withAdult, healthHold){
+    const canRide = (rideHeightOK || withAdult) && !(healthHold);
+    return canRide;
+}
+console.log(houseAlarm(rideHeightOK,withAdult, healthHold));*/
+
+function whoIsInTheMiddle(){}
