6995265c2bfd193e03cca3ba87372fa1fbc7ffda
Hailey Lan <haileylan404@gmail.com>
2025-09-29 11:01:00 -0700

    monday

--- app.js
+++ app.js
@@ -28,12 +28,10 @@ function houseAlarm(rideHeightOK, withAdult, healthHold){
 }
 console.log(houseAlarm(rideHeightOK,withAdult, healthHold));*/
 
-function whoIsInTheMiddle(a, b, c){
+/*function whoIsInTheMiddle(a, b, c){
     if((a > b && a < c) || (a < b && a > c)) return a;
     if((b > a && b < c) || (b < a && b > c)) return b;
     else return c;
-    /*const arr = [a,b,c].sort((a,b) => a - b);
-    return arr[1];*/
 }
 console.log(whoIsInTheMiddle(10, 5, 8))
 
@@ -45,4 +43,36 @@ function telemarketer(a, b, c, d){
     ){return 'ignore';}
     else return 'answer';
 }
-console.log(telemarketer(8, 8, 6, 8))
+console.log(telemarketer(8, 8, 6, 8))*/
+
+//English or French
+/*function engOrFr(num, ...lines){
+    let sCount = 0;
+    let tCount = 0;
+
+    for(i = 0; i < num; i++){
+        for(j = 0; j < lines[i].length; j++){
+            if(lines[i][j] === 's' || lines[i][j] === 'S') sCount++;
+            if(lines[i][j] === 't' || lines[i][j] === 'T') tCount++;
+        }
+    }
+
+    if(tCount > sCount) return 'English';
+    else if(sCount > tCount) return 'French';
+    else return 'French';
+}
+console.log(engOrFr(4, "dans un livre", "Connaissant ce coeur qui doute,", "Tu me dirais de tirer un trait", "Quoi que partir me coute."));*/
+
+//occupiedSpaces
+function occupiedSpaces(num, yest, tdy){
+    let occupiedBothDays = 0;
+
+    for(i = 0; i < num; i++){
+        if(yest[i] === tdy[i] && yest[i] === 'C' & tdy[i] === 'C'){
+            occupiedBothDays++;
+        };
+    }
+    return occupiedBothDays;
+}
+console.log(occupiedSpaces(7, 'CCCCCCC', 'C.C.C.C'));
+
